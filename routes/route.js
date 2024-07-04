57ff09b42d91ce4b8c23e5a78b94698805b52497
Adarsh <adarsh9540984202@gmail.com>
2024-07-04 12:36:24 +0530

    route

--- routes/route.js
+++ routes/route.js
@@ -39,7 +39,7 @@ const generateMCQQuestions = async (topic) => {
   }
 };
 
-router.post('/chat', async (req, res) => {
+router.post('/', async (req, res) => {
   const { topic } = req.body;
 
   if (!topic) {
